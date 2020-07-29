76f069cb5da96a1d2f4ba108d47b106b72d3fd9c
sandmini <39480731+sandmini@users.noreply.github.com>
2020-07-29 18:55:48 +0700

    test 2

--- guestbook.py
+++ guestbook.py
@@ -31,4 +31,4 @@ def process():
     return redirect(url_for('index'))
 if __name__ == '__main__':
     app.debug = True    
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='127.0.0.1', port=80)
