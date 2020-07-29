f9f1554a92e2c0dbb532ffc6bc71ff5cc1a91766
sandmini <39480731+sandmini@users.noreply.github.com>
2020-07-29 19:00:37 +0700

    test 4

--- guestbook.py
+++ guestbook.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column,Integer,String,Date
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:IBMktp38516@node1427-pongpuck.app.ruk-com.cloud:11024/testdb'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:IBMktp38516@10.100.2.32:5432/testdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 class Comments(db.Model):
